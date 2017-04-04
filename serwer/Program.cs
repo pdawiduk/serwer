c5a369a92a23c0158fffe01ddd5f93a94c83118a
Shogun <p.dawiduk@gmail.com>
2017-04-04 13:38:56 +0200

    added method to JSON serialized and deserialized

--- serwer/Program.cs
+++ serwer/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -11,20 +13,23 @@ namespace serwer
     class Program
     {
         const String DISCOVER = "DISCOVER";
-        const int portNumber = 7;
-        const String ipAddress = "239.0.0.222";
-        static IPAddress multicastAddress = IPAddress.Parse(ipAddress);
-        static List<string> serverList = new List<string>();
+        const string PATH = "servers.json";
+        const String IP_ADDRESS = "239.0.0.222";
+        const string ANSWER_YES = "y";
+        const string ANSWER_NO = "n";
+        const int PORT_NUMBER = 7;
+        
+        static IPAddress multicastAddress = IPAddress.Parse(IP_ADDRESS);
+        static List<Server> serverList = new List<Server>();
 
         static void Main(string[] args)
         {
-
-            sendMessage(DISCOVER, portNumber);
-            getServerIPs(DISCOVER, portNumber);
+            loadServersFromJson();
+            saveServersToJson();
 
             Console.WriteLine("adresy IP ");
-            foreach (String strIp in serverList)
-                Console.WriteLine("ip na liscie " + strIp);
+            foreach (Server strIp in serverList)
+                Console.WriteLine("ip na liscie " + strIp.Ip);
             Console.ReadKey();
         }
 
@@ -58,9 +63,13 @@ namespace serwer
                 while (udpClient.Available > 0)
                 {
                     data = udpClient.Receive(ref localEP);
-                    Console.WriteLine("mam serwer");
                     string strData = Encoding.ASCII.GetString(data);
-                    serverList.Add(strData);
+                    serverList.Add(new Server(strData));
+                }
+
+                if (serverList.Count == 0) {
+                    Console.WriteLine(" I don't found any servers !!!!! ");
+                    Console.WriteLine("Check your's internet connect");
                 }
             }
             catch (Exception ex)
@@ -70,6 +79,38 @@ namespace serwer
             }
 
 
+        }
+
+        private static void saveServersToJson()
+        {
+
+            string json = JsonConvert.SerializeObject(serverList, Formatting.Indented);
+            File.WriteAllText(PATH, json);
+        }
+
+        private static void loadServersFromJson()
+        {
+            string fileStream = File.ReadAllText(PATH);
+            List<Server> tmpServers = JsonConvert.DeserializeObject<List<Server>>(fileStream);
+
+            Console.WriteLine("==========================Last finded servers========================");
+            for (int i = 0; i < tmpServers.Count; i++)
+                Console.WriteLine(i + 1 + "    " + tmpServers.ElementAt(i).Ip);
+            Console.WriteLine("=======================Do you want to search new servers y/n ?========================");
+            string answer = Console.ReadLine();
+
+            switch (answer) {
+
+                case ANSWER_YES:
+                    sendMessage(DISCOVER, PORT_NUMBER);
+                    getServerIPs(DISCOVER, PORT_NUMBER);
+                    break;
+
+                case ANSWER_NO:
+                    serverList = tmpServers;
+                    break;
+               
+            }
         }
     }
 }
---
+++ serwer/server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Server
+{
+    private string ip;
+     public string Ip { get
+        {
+            return ip;
+        }
+        set { ip = value; } }
+
+	public Server(String ip)
+	{
+        this.ip = ip;
+    }
+}
